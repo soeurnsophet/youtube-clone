c90528a0a745c181cbc46b8c7051f893b5442061
soeurn sophet <soeurnsophet@gmail.com>
2025-11-01 23:02:09 +0700

    sidebar done 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ import profile from "/assets/images/profile.jpg";
 
 function Header() {
   return (
-    <header className="h-16 w-screen sticksy top-0 left-0 z-50 bg-gray-400/10">
-      <nav className="h-full flex items-center justify-between px-5">
+    <header className="h-16 w-screen sticksy top-0 left-0 z-50 shadow-lg">
+      <nav className="h-full flex items-center justify-between px-5 container">
         {/* left */}
         <div className="center-div">
           <IoMenu className="size-6 mr-2" />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,97 @@
 import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import profile from "/assets/images/profile.jpg";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-50 shadow-2xl min-h-[calc(100vh-4rem)] p-5">
+      <div className="flex flex-col gap-3">
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <AiFillHome className="text-2xl" />
+          <spans className="font-medium">Home</spans>
+        </div>
+      </div>
+
+      <hr className="my-5" />
+      <div>
+        <h1>SUBSCRIBED</h1>
+        <div className="mt-3 space-y-3">
+          <div className="flex items-center justify-start gap-2">
+            <img
+              src={profile}
+              className="size-9 rounded-full object-center aspect-square"
+              alt=""
+            />
+            <spans className="font-medium">PewDiePie</spans>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <img
+              src={profile}
+              className="size-9 rounded-full object-center aspect-square"
+              alt=""
+            />
+            <spans className="font-medium">PewDiePie</spans>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <img
+              src={profile}
+              className="size-9 rounded-full object-center aspect-square"
+              alt=""
+            />
+            <spans className="font-medium">PewDiePie</spans>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <img
+              src={profile}
+              className="size-9 rounded-full object-center aspect-square"
+              alt=""
+            />
+            <spans className="font-medium">PewDiePie</spans>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <img
+              src={profile}
+              className="size-9 rounded-full object-center aspect-square"
+              alt=""
+            />
+            <spans className="font-medium">PewDiePie</spans>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
