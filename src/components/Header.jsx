b058306e19337166fd8d49b7424513eebe4ff794
soeurn sophet <soeurnsophet@gmail.com>
2025-11-02 21:16:48 +0700

    make header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ import { Link } from "react-router-dom";
 
 function Header({ setIsExpand }) {
   return (
-    <header className=" fixed top-0 left-0 h-16 w-full z-50 bg-white shadow-lg ">
-      <nav className="h-full flex items-center justify-between px-5 container">
+    <header className="fixed top-0 left-0 h-16 w-full z-50 bg-white shadow-lg ">
+      <nav className="h-full flex items-center justify-between px-2 sm:px-5 container">
         {/* left */}
         <div className="center-div">
           <IoMenu
@@ -18,31 +18,33 @@ function Header({ setIsExpand }) {
             className="size-6 mr-2 hidden sm:block cursor-pointer"
           />
           <Link to={"/"} className="center-div">
-            <img src={logo} alt="" className="size-10" />
-            <span className="font-bold text-xl tracking-tighter">Youtube</span>
+            <img src={logo} alt="" className="size-8 sm:size-10" />
+            <span className="font-bold text-[0.8rem] text-xl tracking-tighter">
+              Youtube
+            </span>
           </Link>
         </div>
         {/* middle */}
         <div className="basis-2/5">
-          <div className="center-div relative overflow-hidden w-full h-full  border rounded-full">
+          <div className="center-div relative sm:overflow-hidden overflow-hidden sm:w-full sm:h-full  border rounded-full">
             <input
-              className="focus:outline-none pl-4 w-full py-2 pr-5"
+              className="focus:outline-none w-full text-[.8rem] pl-2 sm:pl-4 sm:py-2 sm:pr-5"
               type="search"
               placeholder="Search"
             />
-            <CiSearch className="bg-[#222222] text-white h-10 w-15 p-2" />
+            <CiSearch className="bg-[#222222] text-white h-6 w-6 px-1 sm:h-10 sm:w-15 sn:p-2" />
           </div>
         </div>
         {/* right */}
         <div className="center-div gap-3">
-          <div className="center-div gap-1 bg-gray-900/50 text-white px-3 py-2 rounded-full cursor-pointer">
+          <div className="center-div sr-only sm:not-sr-only gap-1 bg-gray-900/50 text-white sm:px-3 sm:py-2 rounded-full cursor-pointer">
             <FaPlus className="size-" />
             <span className="font-medium">Create</span>
           </div>
-          <PiBellSimpleRingingBold className="w-6 h-6" />
+          <PiBellSimpleRingingBold className="w-6 h-6 hidden sm:block" />
           <img
             src={profile}
-            className="size-9 rounded-full object-center aspect-square"
+            className="size-8 sm:size-9 rounded-full object-center aspect-square"
             alt=""
           />
         </div>
